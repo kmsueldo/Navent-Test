511dacc3dbdb6ff4d690be1a1434388453037239
kmsueldo <kmsueldo@hotmail.com>
2021-04-14 18:04:20 -0300

    Se resuelve el Problema B del test

--- test/test.js
+++ test/test.js
@@ -12,7 +12,31 @@ const pushAndUnshift = (array) => {
 }
 
 /* MODIFICAR LOS VALORES DEL ARRAY PARA PROBAR LA FUNCION Y ASI GENERAR 
-   DISTINTOS CASOS DE PRUEBA                                             */
-let array = [6,7,8,9,10]
+   DISTINTOS CASOS DE PRUEBA                                                       */
 
+let array = [6,7,8,9,10]
 pushAndUnshift(array)
+
+
+
+console.log("\n///////////////////////////////////////////////////////////////////");
+console.log("///////////////////////////////////////////////////////////////////");
+
+
+
+
+/* PROBLEMA B
+   Dadas 2 listas ordenadas, obtener una 3era ordenada:                            */
+
+const orderArray = (array1, array2) => {
+        let joinArray = [...array1, ...array2].sort()
+        console.log("\n Array ordenado", joinArray);
+        return joinArray
+}
+
+/* MODIFICAR LOS VALORES DE LOS 2 ARRAY PARA PROBAR LA FUNCION Y ASI GENERAR 
+   DISTINTOS CASOS DE PRUEBA                                                       */
+   
+let array1 = [1,3,5,8]
+let array2 = [2,3,6,9]
+orderArray(array1, array2)
